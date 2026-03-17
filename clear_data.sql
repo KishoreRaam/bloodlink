82a591ddf08d9b70353645ea2587e0eb0c5af9db
KishoreRaam <221314032+KishoreRaam@users.noreply.github.com>
2026-03-17 19:28:06 +0530

    Remove seed data from `seed.sql` and add `clear_data.sql` to truncate all tables.

---
+++ clear_data.sql
@@ -0,0 +1,11 @@
+USE blood_management_system;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE donation_record;
+TRUNCATE TABLE patient_request;
+TRUNCATE TABLE donor;
+TRUNCATE TABLE blood_bank;
+TRUNCATE TABLE hospital;
+
+SET FOREIGN_KEY_CHECKS = 1;
